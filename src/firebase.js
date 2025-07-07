a799bf01aded413566b6105f68fe88f32ab98532
KenjiTadano <tadanokenji5222@gmail.com>
2025-07-07 14:12:53 +0900

    firebaseの設定を修正

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// src/firebase.js
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebaseの設定
 const firebaseConfig = {
-  apiKey: "AIzaSyDTia_9EMxXFyPa50pYcXB-ttPIKwVVRq4",
-  authDomain: "englishstudy-8403c.firebaseapp.com",
-  projectId: "englishstudy-8403c",
-  storageBucket: "englishstudy-8403c.firebasestorage.app",
-  messagingSenderId: "902319147072",
-  appId: "1:902319147072:web:1a2b05088cf02b7ca677a5",
-  measurementId: "G-L3MQHC3YRT",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Firebaseアプリの初期化
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+// Firestoreの初期化
+const db = getFirestore(app);
+
+export { db }; // dbオブジェクトをエクスポート
